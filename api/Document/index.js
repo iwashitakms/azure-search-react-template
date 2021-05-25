fb00b44e81728b664ff566f514c4257c5cb23f0a
iwashita <iwashita.kms@gmail.com>
2021-05-25 17:33:42 +0900

    fix

---
+++ api/Document/index.js
@@ -0,0 +1,76 @@
+const {
+    BlobServiceClient,
+    StorageSharedKeyCredential,
+    newPipeline
+} = require('@azure/storage-blob');
+
+const sharedKeyCredential = new StorageSharedKeyCredential(
+  process.env.AZURE_STORAGE_ACCOUNT_NAME,
+  process.env.AZURE_STORAGE_ACCOUNT_ACCESS_KEY);
+
+const pipeline = newPipeline(sharedKeyCredential);
+  
+const blobServiceClient = new BlobServiceClient(
+  `https://${process.env.AZURE_STORAGE_ACCOUNT_NAME}.blob.core.windows.net`,
+  pipeline
+);
+
+const streamToBuffer = async (readableStream) => {
+    return new Promise((resolve, reject) => {
+      const chunks = [];
+      readableStream.on('data', (data) => {
+        chunks.push(data instanceof Buffer ? data : Buffer.from(data));
+      });
+      readableStream.on('end', () => {
+        resolve(Buffer.concat(chunks));
+      });
+      readableStream.on('error', reject);
+    });
+  };
+
+module.exports = async function (context, req) {
+
+    try {
+        
+        const fileName = (req.query.fileName || (req.body && req.body.fileName));
+
+        if (!fileName || fileName === "") {
+            throw 'invalid parameter fileName';
+        }
+
+        const filePath = decodeURI(fileName);
+
+        const containerName = process.env["ContainerName"];
+
+        const containerClient = blobServiceClient.getContainerClient(containerName);
+
+        const blockBlobClient = containerClient.getBlockBlobClient(filePath);
+
+        const response = await blockBlobClient.download(0);
+
+        const content = await streamToBuffer(
+            response.readableStreamBody,
+          );
+
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            headers: {
+                "Content-type": response.contentType,
+                "Content-Disposition": encodeURIComponent(filePath)
+            },
+            body: content
+        };
+
+    } catch (error) {
+        context.log.error(error);
+
+        // Creating the HTTP Response
+        context.res = {
+            status: 400,
+            body: {
+                innerStatusCode: error.statusCode || error.code,
+                error: error.details || error.message
+            }
+        };
+    }
+};
--- src/components/Results/Result/Result.js
+++ src/components/Results/Result/Result.js
@@ -2,12 +2,49 @@ import React from 'react';
 
 import './Result.css';
 
+import { saveAs } from "file-saver";
+
+import axios from 'axios';
+
 export default function Result(props) {
+    function getFileName(contentDisposition){
+        let fileName = contentDisposition;
+        //デコードするとスペースが"+"になるのでスペースへ置換します
+        fileName = decodeURI(fileName).replace(/\+/g, " ");
+    
+        return fileName;
+    }
+
+    function download (e) 
+    { 
+        const fileName = e.currentTarget.getAttribute("fileName")
+
+        const body = {
+            fileName: encodeURIComponent(fileName)
+        };
+        axios.post('/api/document', body, {
+                responseType: "blob"
+                })
+                .then(response => {
+                const blob = new Blob([response.data], {
+                    type: response.data.type
+                });
+
+                //レスポンスヘッダからファイル名を取得します
+                const contentDisposition = decodeURI(response.headers["content-disposition"]);
+                const fileName = getFileName(contentDisposition)
+
+                //ダウンロードします
+                saveAs(blob, fileName);
+        });
+    };
+
     return(
         <div>
-            <a href={`/details/${props.document.id}`}>
+            <button onClick={download} fileName={props.document.metadata_storage_name}>{props.document.metadata_storage_name}</button>
+            {/* <a href={`/api/document/${props.document.metadata_storage_name}`}>
                 <h6 className="title-style">{props.document.metadata_storage_name}</h6>
-            </a>
+            </a> */}
             <p>{props.document.content}</p>
         </div>
     );
